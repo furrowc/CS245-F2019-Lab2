b37022c03f5d603b75fa029b2e2624620f36f29f
Caleb Furrow <furrowc@thomas.edu>
2019-09-18 14:39:21 -0400

    All Fixed

--- CS245-F2019-Lab2/CS245-F2019-Lab2.cpp
+++ CS245-F2019-Lab2/CS245-F2019-Lab2.cpp
@@ -15,45 +15,50 @@
 
  // Preprocessor directives
 #include <random>
+#include <vector>
+#include <iostream>
+using std::string;
+using std::cout;
+using std::endl;
 
 
 int main()
 {
 	// Create a vector of strings; add items using a list initializer
-	vector<String> movies = "Holmes & Watson", "Mega Shark vs. Crocosaurus", "C-SPAN: The Movie", "That's My Boy";
+	std::vector<string> movies{ "Holmes & Watson", "Mega Shark vs. Crocosaurus", "C-SPAN: The Movie", "That's My Boy" };
 
 	// Add a 5th string to the end of the vector
-	movies.add("Titanic 2: The Quickening");
+	movies.push_back("Titanic 2: The Quickening");
 
 	// Announce the nominees
-	cout << "And the nominees for Best Picture (cough) are..." >> endl;
+	cout << "And the nominees for Best Picture (cough) are..." << endl;
 
 	// Loop through the vector; print each vector element to console.
-	for (i = 0; i < movies.Length; i++)
+	for (int i = 0; i < movies.size(); i++)
 	{
-		cout << movies.get(i) << endl;
+		cout << movies[i] << endl;
 	}
 
 	// Set up a random number generator seeder
 	std::random_device seed;
 
 	// Create a random integer generator; chooses random ints from 1 to 5 (inclusive)
-	std::uniform_int_distribution<int> rand_dist(1, 6);
+	std::uniform_int_distribution<int> rand_dist(1, 5);
 
 	// Get the next random integer
 	int pick_a_number = rand_dist(seed);
 
 	// Get the item from the vector at the randomly-selected index
-	String winner = movies.get[pick_a_number];
+	string winner = movies[pick_a_number];
 
 	// Convert each charcter of the 'winner' string to UPPERCASE
-	for (char c : winner)
-	{
-		c = c.toUpper();
+	for (auto& c : winner) {
+		c = toupper(c);  
 	}
 
+
 	// Print the UPPERCASE string
-	cout << endl << "And the winner is..." << winner;
+	cout << "And the winner is..." << winner << endl;
 
 	return 0;
 }
